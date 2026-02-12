5b3f6685be812338a771db9ca120b34c1c69252f
Mina <minayeh1347@gmail.com>
2026-02-12 12:06:03 -0500

    line 2

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,12 @@ import './App.css'
 function App() {
 
   return (
+  <>
   <p className="line-1 anim-typewriter">blackjack</p>
+  <div>
+      <p className="line-2 anim-typewriter">beat the odds.</p>
+  </div>
+  </>
   )
 }
 
